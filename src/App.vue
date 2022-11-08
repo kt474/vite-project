22f2a3f51d9c4ff1c276954d785d5daa0c8f7ae9
kevin-tian <kevin.tian@ibm.com>
2022-11-07 21:09:24 -0500

    add simple navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Navbar from "./components/Navbar.vue";
 </script>
 
-<template></template>
+<template>
+  <div class="container mx-auto px-16">
+    <Navbar />
+  </div>
+</template>
 
 <style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <div class="navbar bg-base-100 flex justify-between">
+    <img src="../assets/logo.svg" />
+      <ul>
+        <li class="mx-4 text-lg cursor-pointer hover:text-indigo-900">Home</li>
+        <li class="mx-4 text-lg cursor-pointer hover:text-indigo-900">Documentation</li>
+        <li class="mx-4 text-lg cursor-pointer hover:text-indigo-900">Templates</li>
+        <li class="mx-4 text-lg cursor-pointer hover:text-indigo-900">About</li>
+      </ul>
+  </div>
+</template>
+
+<style scoped></style>
