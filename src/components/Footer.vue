3fe814995b68d1b3686b8d1d2ad206a3918f58a2
kevin-tian <kevin.tian@ibm.com>
2022-12-17 22:24:07 -0500

    add text to main page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,13 @@
 <script setup></script>
 
 <template>
-  <div class="w-full bg-white absolute bottom-0 left-0 px-40 py-16">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat mollitia
-    voluptate alias autem est aut veritatis. Expedita ducimus atque architecto
-    nesciunt earum modi iste, voluptatum voluptates aperiam. Alias, hic eius?
+  <div class="w-full bg-white absolute bottom-0 left-0 px-44 py-16">
+    <img src="../assets/logo.svg" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat mollitia
+      voluptate alias autem est aut veritatis. Expedita ducimus atque architecto
+      nesciunt earum modi iste, voluptatum voluptates aperiam. Alias, hic eius?
+    </p>
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,18 @@
 
 <template>
   <div class="pt-12 pl-12">
-    <p>Lorem Ipsum test</p>
+    <div class="text-center pt-28">
+      <h2 class="text-6xl font-semibold">Website templates</h2>
+      <h2 class="text-6xl font-semibold">In every framework</h2>
+      <p class="pt-8 text-slate-600">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, ab?
+      </p>
+      <p class="pb-8 text-slate-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        repellendus, error facere?
+      </p>
+      <button class="btn btn-primary capitalize">Get started</button>
+    </div>
   </div>
 </template>
 
